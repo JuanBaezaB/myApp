0d646f8a3bee9cf8f4c78f2a46399b1d67dc9919
Juan <51477488+JuanBaezaB@users.noreply.github.com>
2022-12-08 22:20:06 -0300

    Register

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { environment } from "src/environments/environment";
 import { User } from "../models/user.model";
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root',
@@ -8,4 +9,20 @@ import { User } from "../models/user.model";
  export class AuthService{
     constructor(private http: HttpClient){}
 
+    login(user: User){
+        return this.http.post(environment.api_url+'/auth/login', user);
+    }
+
+    Register(user: User){
+        return this.http.post(environment.api_url+'/auth/register', user);
+    }
+
+    logout(user: User){
+        return this.http.post(environment.api_url+'/auth/logout', {});
+    }
+
+    isLoggedIn(){
+        const token = localStorage.getItem("token");
+        return token;
+    }
  }
