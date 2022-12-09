79e5e40f026a569f11188a5f7ee9c777c2a9d72e
Juan <51477488+JuanBaezaB@users.noreply.github.com>
2022-12-09 00:27:08 -0300

    login

--- src/app/page/auth/login/login.page.ts
+++ src/app/page/auth/login/login.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  // @ts-ignore
+  form: FormGroup
 
-  constructor() { }
+  constructor(
+    private loadingCrl: LoadingController,
+    private authService: AuthService,
+    private alertCtrl: AlertController,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+    })
+  }
+
+  async onLogin() {
+    const loading = await this.loadingCrl.create({
+      message: "Please wait ...",
+    });
+    loading.present();
+
+    this.authService.login(this.form.value).subscribe({
+      next: (response: any) => {
+        if (response.success) {
+          console.log(response);
+          loading.dismiss();
+          this.form.reset();
+          localStorage.setItem('token', response.token);
+          localStorage.setItem('user', response.user);
+          this.router.navigateByUrl('/home');
+        }
+
+      },
+      error: (error) => {
+        loading.dismiss();
+        console.error();
+      }
+    });
   }
 
 }
--- src/app/page/auth/register/register.page.ts
+++ src/app/page/auth/register/register.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-
-  constructor() { }
+  // @ts-ignore
+  form: FormGroup
+  constructor(
+    private loadingCrl: LoadingController,
+    private authService: AuthService,
+    private alertCtrl: AlertController,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('',[ Validators.required, Validators.email]),
+      password: new FormControl('',[Validators.required]),
+    })
+  }
+
+  async onRegister(){
+    const loading = await this.loadingCrl.create({
+      message:"Please wait ...",
+    });
+    loading.present();
+
+    this.authService.Register(this.form.value).subscribe({
+      next: () => {
+        loading.dismiss();
+        this.form.reset();
+        this.router.navigateByUrl('/login');
+      },
+      error: (error)=>{
+        loading.dismiss();
+        console.error();
+      }
+    });
   }
 
 }
