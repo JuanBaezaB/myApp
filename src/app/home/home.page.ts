160baa8630fb0f402a2a21bfc78e5b7b4df1e709
JuanBaezaB <51477488+JuanBaezaB@users.noreply.github.com>
2022-12-16 16:23:24 -0300

    onboarding

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public onboardSliders: any[] = []
 
   constructor() {}
+
+  ngOnInit(){
+    this.onboardSliders = [
+      {
+        title:'play the beat',
+        img: 'slide_1',
+        desc: 'Most beginner producer learn creating by simple beats.',
+      },
+      {
+        title:'live the life',
+        img: 'slide_2',
+        desc: 'In our daily lives, we often rush tasks trying to get then finish.',
+      },
+      {
+        title:'capture the moment',
+        img: 'slide_3',
+        desc: 'You are not alone. You have unique ability to go to another world.',
+      },
+
+    ];
+  }
+
+  public goBack(){
+
+  }
+  public skipBtn(){
+
+  }
+  public goNext(){
+
+  }
 
 }
