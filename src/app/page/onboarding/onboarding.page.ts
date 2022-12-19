5724ccb638f30f7d2317bb86e1c51fff84e32978
JuanBaezaB <51477488+JuanBaezaB@users.noreply.github.com>
2022-12-19 12:27:54 -0300

    onboarding design

--- src/app/page/onboarding/onboarding.page.ts
+++ src/app/page/onboarding/onboarding.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-onboarding',
@@ -7,15 +8,48 @@ import { Router } from '@angular/router';
   styleUrls: ['./onboarding.page.scss'],
 })
 export class OnboardingPage implements OnInit {
+  Slides: any [] = []
 
   constructor(
     private router: Router
   ) { }
+  
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400,
+
+  };
 
   ngOnInit() {
+    this.Slides = [
+      {
+        title: 'Start to invest for your future!',
+        text: 'Ex totam praesentium incidunt aut.',
+        img: 'step-1.svg'
+      },
+      {
+        title: 'Follow our tips to achieve success!',
+        text: 'Ex totam praesentium incidunt aut.',
+        img: 'step-2.svg',
+        imgBottom: true,
+      },
+      {
+        title: 'Keep your investment safe',
+        text: 'Ex totam praesentium incidunt aut.',
+        img: 'step-3.svg'
+      },
+    ]
   }
 
-  async begin() {
+  navigate(slide: any, index: any) {
+    slide.slideTo(index)
+  }
+
+  skip(slide: any, index: any){
+    slide.slideTo(index)
+  }
+
+  async start() {
     localStorage.setItem('onboarding', 'true');
     this.router.navigateByUrl('/login');
   }
